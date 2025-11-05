d8e7fca9a6d4d5e05c02cd59dc4ec07c24cc057f
LETICYA SILVA DIAS <leticya_dias@estudante.sesisenai.org.br>
2025-11-04 21:53:26 -0300

    modificações

--- backend/app.js
+++ backend/app.js
@@ -76,71 +76,77 @@ app.put("/usuario/:id", async (req, res) => {
     }
 });
 
-    app.post("/livro", async (req, res) => {
-        try {
-            const { body } = req;
-            const [results] = await pool.query(
-                "INSERT INTO livro (titulo, autor, categoria, paginas, tempo_leitura,nota) VALUES (?, ?, ?, ?, ?,?,?)",
-                [
-                    body.livro,
-                    body.autor,
-                    body.categoria,
-                    body.paginas,
-                    body.tempo_leitura,
-                    body.nota,
-                    body.resenha,
-                ]
-            );
-            const [livroAdicionado] = await pool.query(
-                "SELECT * FROM livro WHERE id_livro=?",
-                results.insertId
-            );
-            res.status(201).json(livroAdicionado);
-        } catch (error) {
-            console.log(error);
-        }
-    });
+app.post("/livro", async (req, res) => {
+    try {
+        const { body } = req;
+        const [results] = await pool.query(
+            "INSERT INTO livro (titulo, autor, categoria, paginas, tempo_leitura,nota) VALUES (?, ?, ?, ?, ?,?,?)",
+            [
+                body.titulo,
+                body.autor,
+                body.categoria,
+                body.paginas,
+                body.tempo_leitura,
+                body.nota,
+                body.resenha,
+            ]
+        );
+        const [livroAdicionado] = await pool.query(
+            "SELECT * FROM livro WHERE id_livro=?",
+            results.insertId
+        );
+        res.status(201).json(livroAdicionado);
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-    // app.get("/livro", async (req, res) => {
-    //     const { query } = req;
-    //     const pagina = Number(query.pagina) - 1
-    //     const quantidade = Number(query.quantidade)
-    //     const offset = pagina * quantidade
-      
-    //     const [results] = await pool.query(`
-    //       SELECT
-    //         livro.id_livro,
-    //         livro.categoria,
-    //         livro.horas_trabalhadas,
-    //         livro.linhas_codigo,
-    //         livro.bugs_corrigidos,
-    //         livro.id_user,
-    //         (SELECT COUNT(*) FROM devhub.like WHERE devhub.like.id_log = livro.id) AS likes,
-    //         (SELECT COUNT(*) FROM devhub.comment WHERE devhub.comment.id_log = livro.id) as qnt_comments
-    //       FROM
-    //         devhub.livro 
-    //       left JOIN devhub.like
-    //       ON devhub.like.id_log = devhub.livro.id
-    //       LEFT JOIN devhub.comment
-    //       ON devhub.comment.id_log = devhub.livro.id
-    //       LEFT JOIN devhub.usuario
-    //       ON devhub.usuario.id = devhub.livro.id_user
-    //       GROUP BY
-    //       livro.id,
-    //         livro.categoria,
-    //         livro.horas_trabalhadas,
-    //         livro.linhas_codigo,
-    //         livro.bugs_corrigidos,
-    //         livro.id_user
-    //       ORDER BY devhub.livro.id asc
-    //         LIMIT ?
-    //         OFFSET ?
-    //       `, [quantidade, offset]);
-    //     res.send(results);
-    //   });
-      
+app.get("/livro", async (req, res) => {
+    const { query } = req;
+    const pagina = Number(query.pagina) - 1
+    const quantidade = Number(query.quantidade)
+    const offset = pagina * quantidade
+
+    const [results] = await pool.query(`
+    SELECT
+    livro.id_livro,
+    livro.titulo,
+    livro.autor,
+    livro.categoria,
+    livro.paginas,
+    livro.tempo_leitura,
+    livro.nota,
+    livro.nota,
+    livro.id_user,
+    (SELECT COUNT(*) FROM biblioteca.likes WHERE biblioteca.likes.id_livro = livro.id_livro) AS likes,
+    (SELECT COUNT(*) FROM biblioteca.comentario WHERE biblioteca.comentario.id_livro = livro.id_livro) as qnt_comments
+    FROM
+    biblioteca.livro 
+    left JOIN biblioteca.likes
+    ON biblioteca.likes.id_livro = biblioteca.livro.id_livro
+    LEFT JOIN biblioteca.comentario
+    ON biblioteca.comentario.id_livro = biblioteca.livro.id_user
+    LEFT JOIN biblioteca.usuario
+    ON biblioteca.usuario.id_user = biblioteca.livro.id_user
+    GROUP BY
+    livro.id_livro,
+    livro.titulo,
+    livro.autor,
+    livro.categoria,
+    livro.paginas,
+    livro.tempo_leitura,
+    livro.nota,
+    livro.nota,
+    livro.id_user
+    ORDER BY biblioteca.livro.id_livro asc
+    LIMIT ?
+    OFFSET ?
+    `, [quantidade, offset]);
+    res.send(results);
+});
 
 
-    app.listen(3000, () => {
-        console.log(`Servidor rodando na porta: http://localhost:3000`);
-    });
+
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta: http://localhost:3000`);
+});
