a5821938126aa1188696bb64137e5f780d2be401
Leleh06 <leticyasilvad1608@gmail.com>
2025-11-05 22:26:07 -0300

    Backend com Frontend

--- biblioteca/app.js
+++ biblioteca/app.js
@@ -76,18 +76,43 @@ app.put("/usuario/:id", async (req, res) => {
     }
 });
 
+app.post("/login", async (req, res) => {
+    try {
+      const { body } = req;
+  
+      const [usuario] = await pool.query(
+        "Select * from usuario WHERE email=? and senha=?",
+        [body.email, body.senha]
+      );
+  
+      if (usuario.length > 0) {
+        return res.status(200).json({
+          message: "Usuario logado",
+          dados: usuario,
+        });
+      } else {
+        return res.status(404).send("Email ou senha errados!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
+// Livro
+
 app.post("/livro", async (req, res) => {
     try {
         const { body } = req;
         const [results] = await pool.query(
-            "INSERT INTO livro (titulo, autor, categoria, paginas, tempo_leitura,nota) VALUES (?, ?, ?, ?, ?,?,?)",
+            "INSERT INTO livro ( titulo, autor, categoria, paginas, tempo_leitura, nota, resenha) VALUES (?, ?, ?, ?, ?, ?, ?)",
             [
                 body.titulo,
                 body.autor,
                 body.categoria,
-                body.paginas,
-                body.tempo_leitura,
-                body.nota,
+                parseInt(body.paginas),
+                parseInt(body.tempo_leitura),
+                parseInt(body.nota),
                 body.resenha,
             ]
         );
@@ -101,47 +126,8 @@ app.post("/livro", async (req, res) => {
     }
 });
 
-app.get("/livro", async (req, res) => {
-    const { query } = req;
-    const pagina = Number(query.pagina) - 1
-    const quantidade = Number(query.quantidade)
-    const offset = pagina * quantidade
-
-    const [results] = await pool.query(`
-    SELECT
-    livro.id_livro,
-    livro.titulo,
-    livro.autor,
-    livro.categoria,
-    livro.paginas,
-    livro.tempo_leitura,
-    livro.nota,
-    livro.nota,
-    livro.id_user,
-    (SELECT COUNT(*) FROM biblioteca.likes WHERE biblioteca.likes.id_livro = livro.id_livro) AS likes,
-    (SELECT COUNT(*) FROM biblioteca.comentario WHERE biblioteca.comentario.id_livro = livro.id_livro) as qnt_comments
-    FROM
-    biblioteca.livro 
-    left JOIN biblioteca.likes
-    ON biblioteca.likes.id_livro = biblioteca.livro.id_livro
-    LEFT JOIN biblioteca.comentario
-    ON biblioteca.comentario.id_livro = biblioteca.livro.id_user
-    LEFT JOIN biblioteca.usuario
-    ON biblioteca.usuario.id_user = biblioteca.livro.id_user
-    GROUP BY
-    livro.id_livro,
-    livro.titulo,
-    livro.autor,
-    livro.categoria,
-    livro.paginas,
-    livro.tempo_leitura,
-    livro.nota,
-    livro.nota,
-    livro.id_user
-    ORDER BY biblioteca.livro.id_livro asc
-    LIMIT ?
-    OFFSET ?
-    `, [quantidade, offset]);
+app.get("/livro", (req, res) => {
+    const [results] = pool.query("SELECT * FROM livro");
     res.send(results);
 });
 
